c7c68217733bb430d28137d93a7ec8a6d95b90ec
rasren <rasren@erst.com>
2025-12-09 13:41:02 +0100

    padding right

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,6 +89,7 @@ def evaluate_case():
                     line-height: 1 !important;
                     padding-top: 2px !important;
                     padding-bottom: 2px !important;
+                    padding-right: 2px !important;
                     background-color: white;
                     color: black;
                 }
