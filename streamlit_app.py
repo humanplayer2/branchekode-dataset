3507c67f6bcc90e79028b5896776c556ee69f727
rasren <rasren@erst.com>
2025-12-09 12:43:41 +0100

    97

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,8 +80,8 @@ def evaluate_case():
                 /* items in multiselect container */
                 .stMultiSelect [data-baseweb=select] span{
                     white-space: normal !important;  /* allows line breaks */
-                    min-width: 98%;
-                    max-width: 98%;
+                    min-width: 97%;
+                    max-width: 97%;
                     height: 2.4em;
                     font-size: 1rem;
                     line-height: 1 !important;
