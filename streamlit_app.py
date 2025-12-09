992d04f23cb8c93df55456c8c55076979f4090ad
rasren <rasren@erst.com>
2025-12-09 12:17:18 +0100

    2em heigth

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,7 +82,7 @@ def evaluate_case():
                     white-space: normal !important;  /* allows line breaks */
                     min-width: 900px;
                     max-width: 900px;
-                    height: 1em;
+                    height: 2em;
                     font-size: 1rem;
                 }
             </style>
